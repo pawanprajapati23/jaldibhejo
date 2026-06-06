6f77d6b31c0b98a24a7ae1694b0f166e5c833786
pawanprajapati23 <pavnkumarprajapati2000@gmail.com>
2026-06-06 18:51:14 +0530

    fix: add free TURN servers to resolve mobile data connection hang

--- frontend/src/lib/WebRTCEngine.ts
+++ frontend/src/lib/WebRTCEngine.ts
@@ -191,12 +191,22 @@ export class WebRTCEngine {
     this.peerConnection = new RTCPeerConnection({
       iceServers: [
         { urls: 'stun:stun.l.google.com:19302' },
-        { urls: 'stun:stun1.l.google.com:19302' },
-        { urls: 'stun:stun2.l.google.com:19302' },
-        { urls: 'stun:stun3.l.google.com:19302' },
-        { urls: 'stun:stun4.l.google.com:19302' },
-        { urls: 'stun:stun.services.mozilla.com' },
         { urls: 'stun:global.stun.twilio.com:3478' },
+        {
+          urls: 'turn:openrelay.metered.ca:80',
+          username: 'openrelayproject',
+          credential: 'openrelayproject'
+        },
+        {
+          urls: 'turn:openrelay.metered.ca:443',
+          username: 'openrelayproject',
+          credential: 'openrelayproject'
+        },
+        {
+          urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+          username: 'openrelayproject',
+          credential: 'openrelayproject'
+        }
       ],
       iceCandidatePoolSize: 10,
       bundlePolicy: 'max-bundle',
